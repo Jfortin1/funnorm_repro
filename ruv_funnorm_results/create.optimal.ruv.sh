314b8fa1a84fda96d7b4caee602894c48668e584
Jean-Philippe Fortin <jfortin@jhpce02.cm.cluster>
2014-06-05 19:25:01 -0400

    Added RUV txt files

---
+++ ruv_funnorm_results/create.optimal.ruv.sh
@@ -0,0 +1,2 @@
+cd /amber1/archive/sgseq/workspace/hansen_lab1/funnorm_repro/ruv_funnorm_results
+Rscript create.optimal.ruv.R $1 
---
+++ ruv_results/create.tuning.plots.data.2.sh
@@ -0,0 +1,2 @@
+cd /amber1/archive/sgseq/workspace/hansen_lab1/funnorm_repro/ruv_results
+Rscript create.tuning.plots.data.2.R $1 
