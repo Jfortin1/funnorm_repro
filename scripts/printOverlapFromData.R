534fff81bf69611a10378836026fa0ef89f36df3
Jean-Philippe Fortin <jfortin@jhpce02.cm.cluster>
2014-06-12 15:51:55 -0400

    Updated scripts

--- scripts/printOverlapFromData.R
+++ scripts/printOverlapFromData.R
@@ -1,12 +1,9 @@
-  
-  
-  
   printOverlapFromData <- function(data, 
                                xcutoff, ycutoff = 0, ycutoff2=1,
                                main, 
                                colors, 
                                lty,
-                               names){
+                               names,lwd=rep(1,length(colors))){
   	
 
 
@@ -23,17 +20,7 @@
 		    lines(data$sequences[[i]],data$overlaps[[i]],col=colors[i],lty=lty[i], lwd=2)
 		}
 		
-		
-		# # Plot diagonal line for complete ROC curve
-		# if (xcutoff == 1){
-			# abline(a=0,b=1,lty=3)
-		# }
-		
-		# # Plot vertical lines for specificity cutoff
-		# if (xcutoff==0.1){
-			# abline(v=0.01, lty=3)
-			# abline(v=0.05, lty=1)
-		# }
+
 		
 		 legend("bottomright",names,lwd=2,col=colors, lty=lty, cex=1, bty="n")
 
