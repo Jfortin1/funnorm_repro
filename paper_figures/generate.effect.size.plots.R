68de479126e7433e88f48a9578c775f70c6b94a8
jfortin <jfortin@jhsph.edu>
2014-07-04 16:08:11 -0400

    Updated effect size script

--- paper_figures/generate.effect.size.plots.R
+++ paper_figures/generate.effect.size.plots.R
@@ -7,6 +7,8 @@ dmpsDir <- "/amber1/archive/sgseq/workspace/hansen_lab1/funnorm_repro/dmps"
 setwd(dmpsDir)
 
 
+ylab = expression(paste(Delta,beta))
+
 load("dmps_dis_ontario_ebv.Rda")
 dis <- dmps
 load("dmps_val_ontario_ebv.Rda")
@@ -33,7 +35,7 @@ pdf("Lympho_Effect_Size.pdf", height=5, width=3.5)
 names7 <- c("Raw", "Quantile", "Funnorm","dasen", "SWAN", "noob", "BMIQ","Funnorm w noob")
 colors7 <- c("white", "deepskyblue2", "deeppink4",  "olivedrab", "red","orange", "slateblue4","deeppink1")
 par(bty="n")
-boxplot(new.diffs, ylim=c(0,0.5), col=colors7, names = names7, ylab="Effect size",
+boxplot(new.diffs, ylim=c(0,0.5), col=colors7, names = names7, ylab=ylab,
 	xaxt="n", xlab="", bty="n", outline=FALSE)
 axis(side=1,labels=names7,las=2, at=1:8, cex.axis=0.6)
 abline(h=median, lty=3, lwd=2)
@@ -74,7 +76,7 @@ pdf("KIRC_Effect_Size.pdf", height=5, width=3.5)
 names7 <- c("Raw", "Quantile", "Funnorm","dasen", "SWAN", "noob", "BMIQ","Funnorm w noob")
 colors7 <- c("white", "deepskyblue2", "deeppink4",  "olivedrab", "red","orange", "slateblue4","deeppink1")
 par(bty="n")
-boxplot(new.diffs, ylim=c(0,0.3), col=colors7, names = names7, ylab="Effect size",
+boxplot(new.diffs, ylim=c(0,0.3), col=colors7, names = names7, ylab=ylab,
 	xaxt="n", xlab="",bty="n", outline=FALSE)
 abline(h=median, lty=3, lwd=2)
 axis(side=1,labels=names7,las=2, at=1:8, cex.axis=0.6)
@@ -115,7 +117,7 @@ pdf("Blood_Effect_Size.pdf", height=5, width=3.5)
 names7 <- c("Raw", "Quantile", "Funnorm","dasen", "SWAN", "noob", "BMIQ","Funnorm w noob")
 colors7 <- c("white", "deepskyblue2", "deeppink4",  "olivedrab", "red","orange", "slateblue4","deeppink1")
 par(bty="n")
-boxplot(new.diffs, ylim=c(0,0.05), col=colors7, names = names7, ylab="Effect size",
+boxplot(new.diffs, ylim=c(0,0.05), col=colors7, names = names7, ylab=ylab,
 	xaxt="n", xlab="",bty="n", outline=FALSE)
 abline(h=median, lty=3, lwd=2)
 axis(side=1,labels=names7,las=2, at=1:8, cex.axis=0.6)
