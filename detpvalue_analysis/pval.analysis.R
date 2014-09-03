f084d24c1ebe0c1aa6c3d609a2b52a266f37926d
jfortin <jfortin@jhsph.edu>
2014-09-03 16:46:42 -0400

    Finished pval analysis

--- detpvalue_analysis/pval.analysis.R
+++ detpvalue_analysis/pval.analysis.R
@@ -2,19 +2,33 @@ library(minfi)
 
 funnormDir <- "/amber1/archive/sgseq/workspace/hansen_lab1/funnorm_repro"
 rawDir <- paste0(funnormDir,"/raw_datasets")
+detDir <- paste0(funnormDir,"/detpvalue_analysis")
 dataset_names <- c("ontario_ebv","ontario_blood","kirc")
 dataset_names <- c(dataset_names,"aml","ontario_gender")
 dataset_names <- paste0("rgset_", dataset_names, ".Rda")
 names <- gsub(".Rda","",dataset_names)
 setwd(rawDir)
 
-i=5
-load(dataset_names[i])
-rgset <- get(names[i])
-p <- detectionP(rgset)
-cutoff = 0.01
-failed <- p > cutoff
-means <- colMeans(failed)
+for (i in 1:5){
+	load(dataset_names[i])
+	rgset <- get(names[i])
+	p <- detectionP(rgset)
+	cutoff = 0.01
+	failed <- p > cutoff
+	means <- 1-colMeans(failed)
+	detDir <- paste0(funnormDir,"/detpvalue_analysis")
+	setwd(detDir)
+	save(means, file=paste0("colMeans.",i,".Rda"))
+	sum(means > 0.95)
+}
+
+
+
+# 1. ontario_ebv: All greater than 0.97
+# 2. ontario_blood: All greater than 0.91
+# 3. kirc: All greater than 0.98
+# 4. aml: All greater than 0.99
+# 5. gender: All greater than 0.96
 
 
 
